eb84d1d1fc72ffa223db8ad505c1dda4af7de465
monishankarPodder <monishankar.podder@gmail.com>
2025-06-08 02:24:27 +0530

    Add files via upload

---
+++ product/src/main/java/com/example/product/entity/Product.java
@@ -0,0 +1,17 @@
+package com.example.product.entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double price;
+}
